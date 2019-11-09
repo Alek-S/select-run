0a6f1168c4d5cade018c5b89f6ca9d0d581246a9
Alek Shnayder <alek.shnayder@gmail.com>
2019-11-09 06:19:50 -0600

    minor: whitespace

--- cli.js
+++ cli.js
@@ -17,7 +17,7 @@ const packageJsonPath = path.join(process.cwd(), 'package.json');
 let packageJson;
 try{
 	packageJson = require(packageJsonPath);
-} 
+}
 catch(error) {
 	// no package.json found
 	errorMsg('package.json not found, you in the right directory?')
@@ -35,8 +35,8 @@ userInterview();
  * start the user interview with checkbox multi-select
  */
 function userInterview () {
-	const interviewMessage = 
-		`Select scripts -- (Press ${chalk.cyan('<space>')} to select,` + 
+	const interviewMessage =
+		`Select scripts -- (Press ${chalk.cyan('<space>')} to select,` +
 		` ${chalk.cyan('<return>')} to complete)` +
 		`\nfilter: `;
 
@@ -49,17 +49,17 @@ function userInterview () {
 			searchable: true,
 			source: (_answersSoFar, input) => {
 				input = input || '';
-	
+
 				return new Promise(function(resolve) {
-	
+
 				const fuzzyResult = fuzzy.filter(input, Object.keys(packageJson.scripts));
-	
+
 				const data = fuzzyResult.map(function(element) {
 					return element.original;
 				});
-	
+
 				resolve(data);
-				
+
 				});
 			},
 			choices: Object.keys(packageJson.scripts)
